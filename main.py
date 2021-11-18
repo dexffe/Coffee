808b413b4b9105704489ea84277bb902476db994
dexffe <dexffe@gmail.com>
2021-11-18 20:12:44 +0600

    Nice

---
+++ main.py
@@ -0,0 +1,29 @@
+import sys
+import sqlite3
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QTableWidgetItem
+from PyQt5 import uic
+
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main.ui', self)
+        self.setWindowTitle('Coffee')
+
+        connection = sqlite3.connect("coffee.sqlite")
+        query = '''SELECT * FROM Coffee'''
+        res = connection.cursor().execute(query).fetchall()
+        list = ['ID', 'название сорта', 'степень обжарки', 'молотый/в зернах', 'описание вкуса', 'цена', 'объем упаковки']
+        self.table.setColumnCount(len(list))
+        self.table.setHorizontalHeaderLabels(list)
+        for i, row in enumerate(res):
+            self.table.setRowCount(self.table.rowCount() + 1)
+            for j, elem in enumerate(row):
+                self.table.setItem(i, j, QTableWidgetItem(str(elem)))
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec_())
